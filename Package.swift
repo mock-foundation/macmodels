f143882d4fcbec55ac6fe9eb8519d8500ee00197
GGorAA <ggoraa1029@gmail.com>
2022-07-08 21:25:12 +0300

    chore: update versions Swift > 5.6 macOS > 10.15 iOS > 13 Mac Catalyst > 13 tvOS > 13 watchOS > 6

--- Package.swift
+++ Package.swift
@@ -1,9 +1,16 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.6
 
 import PackageDescription
 
 let package = Package(
     name: "MacModels",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .macCatalyst(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6)
+    ],
     products: [
         .library(
             name: "MacModels",
@@ -13,7 +20,11 @@ let package = Package(
     targets: [
         .target(
             name: "MacModels",
-            dependencies: []),
+            dependencies: [],
+            resources: [
+                .copy("Resources/models.json")
+            ]
+        ),
         .testTarget(
             name: "MacModelsTests",
             dependencies: ["MacModels"]),
