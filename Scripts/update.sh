0bc981d67f886d0611c24f0d7d31e665989be9e5
GGorAA <ggoraa1029@gmail.com>
2022-07-08 21:25:33 +0300

    chore: fix styling in update.sh

--- Scripts/update.sh
+++ Scripts/update.sh
@@ -1,6 +1,7 @@
 # Updates the models.json file
 
 git clone https://github.com/phimage/MacModelDump.git
-
 cd MacModelDump
+
+# Generates the list of devices, prettifies it, and then saves to models.json file
 swift run modelappledump json | python3 -m json.tool > ../Sources/MacModels/Resources/models.json
