87e6c13fade948d7d8dff2a07dcca6e4289e88c0
GGorAA <ggoraa1029@gmail.com>
2022-07-11 16:31:17 +0300

    fix: possibly fixed UpdateModelsCommand not building properly

--- Sources/UpdateModelsCommand/main.swift
+++ Sources/UpdateModelsCommand/main.swift
@@ -9,11 +9,16 @@ import Foundation
 import SharedModels
 import AppleScraper
 
-let modelsFileURL = URL(fileURLWithPath: "Sources/MacModels/Resources/models.json")
-let encoder = JSONEncoder()
-encoder.outputFormatting = .prettyPrinted
-
-let scraped = try await Scraper.scrape(for: "all")
-let newContents = try encoder.encode(scraped)
-
-try newContents.write(to: modelsFileURL, options: .atomic)
+@main
+struct App {
+    static func main() async throws {
+        let modelsFileURL = URL(fileURLWithPath: "Sources/MacModels/Resources/models.json")
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+        
+        let scraped = try await Scraper.scrape(for: "all")
+        let newContents = try encoder.encode(scraped)
+        
+        try newContents.write(to: modelsFileURL, options: .atomic)
+    }
+}
